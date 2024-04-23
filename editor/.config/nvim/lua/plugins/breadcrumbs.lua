4c3228683ff4a4de54d296d81df0673339d8214a
Jesse Rusak <rusak.jesse@gmail.com>
2024-04-23 19:14:12 -0400

    feat(lsp/yamlls): working schema for breadcrumbs

--- editor/.config/nvim/lua/plugins/breadcrumbs.lua
+++ editor/.config/nvim/lua/plugins/breadcrumbs.lua
@@ -1,5 +1,15 @@
 return {
   {
+    'nvimdev/lspsaga.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('lspsaga').setup({})
+    end,
+  },
+  {
     "utilyre/barbecue.nvim",
     name = "barbecue",
     version = "*",
--- editor/.config/nvim/lua/plugins/lsp.lua
+++ editor/.config/nvim/lua/plugins/lsp.lua
@@ -119,6 +119,20 @@ return {
 				},
 			})
 
+			-- YAML
+			require("lspconfig")["yamlls"].setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					yaml = {
+						schemas = {
+							["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml"] =
+							"/*",
+						},
+					},
+				},
+			})
+
 			-- Python
 			require("lspconfig")["pylsp"].setup({
 				on_attach = on_attach,
