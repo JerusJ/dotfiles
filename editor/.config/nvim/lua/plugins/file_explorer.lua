9b688f30e7892a3f14939bcc105b0de453cb0ec5
Jesse Rusak <rusak.jesse@gmail.com>
2024-05-03 12:11:16 -0400

    feat(tmux): navigate between nvim/tmux

--- editor/.config/nvim/lua/plugins/file_explorer.lua
+++ editor/.config/nvim/lua/plugins/file_explorer.lua
@@ -1,25 +1,42 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        local oil = require("oil")
-      oil.setup({
-        default_file_explorer = true,
-        columns = {
-          "icon",
-          "permissions",
-          "size",
-        },
-        view_options = {
-          show_hidden = true,
-        }
-      })
+	{
+		"stevearc/oil.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local oil = require("oil")
+			oil.setup({
+				default_file_explorer = true,
+				columns = {
+					"icon",
+					"permissions",
+					"size",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
 
-        local map = require("helpers.keys").map
-        map("n", "<leader>.", oil.open, "Open Oil")
-    end,
-  }
+			local map = require("helpers.keys").map
+			map("n", "<leader>.", oil.open, "Open Oil")
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 }
