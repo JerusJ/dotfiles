99ec43b4b4858e0d62036b1e5a4c19beed9d5f52
Jesse Rusak <rusak.jesse@gmail.com>
2024-05-08 09:59:24 -0400

    feat(lsp): more common language servers

--- editor/.config/nvim/lua/custom/plugins/telescope.lua
+++ editor/.config/nvim/lua/custom/plugins/telescope.lua
@@ -5,14 +5,20 @@ return {
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
 			require("telescope").setup({
 				pickers = {
-					find_files = {
-						hidden = true,
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--no-ignore-vcs",
+						"-g",
+						"!**/.git/*",
+						"-g",
+						"!**/node_modules/*",
 					},
 				},
 				defaults = {
@@ -32,25 +38,25 @@ return {
 			vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "Open buffers" })
 			vim.keymap.set("n", "<leader>/", function() end, { desc = "Search in current buffer" })
 
-			vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "Files"})
-			vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "Help"})
-            vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "Current word"})
-            vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc ="Grep"})
-            vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc ="Diagnostics"})
+			vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "Files" })
+			vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "Help" })
+			vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "Current word" })
+			vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "Grep" })
+			vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "Diagnostics" })
 			vim.keymap.set(
 				"n",
 				"<leader>sr",
 				"<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
-				{ desc= "Search Git Worktrees" }
+				{ desc = "Search Git Worktrees" }
 			)
 			vim.keymap.set(
 				"n",
 				"<leader>sR",
 				"<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
-				{ desc ="Create Git Worktree"}
+				{ desc = "Create Git Worktree" }
 			)
 
-			vim.keymap.set("n", "<C-p>", require("telescope.builtin").keymaps, { desc ="Search keymaps"})
+			vim.keymap.set("n", "<C-p>", require("telescope.builtin").keymaps, { desc = "Search keymaps" })
 		end,
 	},
 }
