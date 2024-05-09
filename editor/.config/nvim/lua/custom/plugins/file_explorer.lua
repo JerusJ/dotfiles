a6bffea92b7f993c3d7492fcfa2d0f39800a70f8
Jesse Rusak <rusak.jesse@gmail.com>
2024-05-08 20:12:05 -0400

    feat(golang/dap): working config with launch.json
    Need to start Neovim from root of the repo

--- editor/.config/nvim/lua/custom/plugins/file_explorer.lua
+++ editor/.config/nvim/lua/custom/plugins/file_explorer.lua
@@ -8,11 +8,12 @@ return {
 			local oil = require("oil")
 			oil.setup({
 				default_file_explorer = true,
-				columns = {
-					"icon",
-					"permissions",
-					"size",
-				},
+				skip_confirm_for_simple_edits = true,
+				-- columns = {
+				-- 	"icon",
+				-- 	"permissions",
+				-- 	"size",
+				-- },
 				view_options = {
 					show_hidden = true,
 				},
--- editor/.config/nvim/lua/custom/plugins/golang.lua
+++ editor/.config/nvim/lua/custom/plugins/golang.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "leoluz/nvim-dap-go",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
-  },
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"leoluz/nvim-dap-go",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
+	},
 }
