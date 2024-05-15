3acedece42693d93bcadd3039de8e519203407dd
Jesse Rusak <rusak.jesse@gmail.com>
2024-05-15 10:59:47 -0400

    fix(breadcrumbs): disable lightbulb
    This appears on every line and is VERY annoying

--- editor/.config/nvim/lua/custom/plugins/breadcrumbs.lua
+++ editor/.config/nvim/lua/custom/plugins/breadcrumbs.lua
@@ -1,44 +1,52 @@
 return {
-  {
-    'nvimdev/lspsaga.nvim',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('lspsaga').setup({})
-    end,
-  },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-      -- configurations go here
-    },
-    config = function()
-      require("barbecue").setup({
-        create_autocmd = false, -- prevent barbecue from updating itself automatically
-      })
+	{
+		"nvimdev/lspsaga.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lspsaga").setup({
+				lightbulb = {
+					enable = false,
+					sign = false,
+				},
+				beacon = {
+					enable = false,
+				},
+			})
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			-- configurations go here
+		},
+		config = function()
+			require("barbecue").setup({
+				create_autocmd = false, -- prevent barbecue from updating itself automatically
+			})
 
-      vim.api.nvim_create_autocmd({
-        "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
-        "BufWinEnter",
-        "CursorHold",
-        "InsertLeave",
+			vim.api.nvim_create_autocmd({
+				"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+				"BufWinEnter",
+				"CursorHold",
+				"InsertLeave",
 
-        -- include this if you have set `show_modified` to `true`
-        -- "BufModifiedSet",
-      }, {
-        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-        callback = function()
-          require("barbecue.ui").update()
-        end,
-      })
-    end,
-  },
+				-- include this if you have set `show_modified` to `true`
+				-- "BufModifiedSet",
+			}, {
+				group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+				callback = function()
+					require("barbecue.ui").update()
+				end,
+			})
+		end,
+	},
 }
