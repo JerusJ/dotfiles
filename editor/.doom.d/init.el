0f008129e82e07bae205b9e89dea04e8dc6b713e
Jesse Rusak <rusak.jesse@gmail.com>
2024-09-10 17:39:11 +0000

    fix(arch/playwright): resolve last piece of missing deps
    Also add some minor makepkg enhancements for cores (although MAKEFLAGS
    is still commented by default, as some AUR packages do not respect/like
    it.)

--- editor/.doom.d/init.el
+++ editor/.doom.d/init.el
@@ -100,7 +100,7 @@
        (lookup
         +docsets
         +dictionary)              ; navigate your code and its documentation
-       (lsp +eglot)
+       lsp
        (magit +forge)              ; a git porcelain for Emacs
        make                        ; run make tasks from Emacs
        ;;pass                      ; password manager for nerds
