82100f820992fd893c4668ec7974e8d3a7ae3ee2
Jesse Rusak <rusak.jesse@gmail.com>
2024-09-19 20:26:53 +0000

    fix(neovim): no execute please...

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -98,9 +98,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- do this on the bash script you download from the internet
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
