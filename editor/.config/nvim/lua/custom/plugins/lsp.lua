921b890b70b5ecd1fd748daceaeb0c5e3ec878d1
Jesse Rusak <rusak.jesse@gmail.com>
2024-09-19 15:13:38 +0000

    feat(lsp): context

--- editor/.config/nvim/lua/custom/plugins/lsp.lua
+++ editor/.config/nvim/lua/custom/plugins/lsp.lua
@@ -24,19 +24,6 @@ return { -- LSP Configuration & Plugins
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					-- Automaatically Setup signature Help
-					local bufnr = event.buf
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if vim.tbl_contains({ "null-ls" }, client.name) then -- blacklist lsp
-						return
-					end
-					require("lsp_signature").on_attach({
-						bind = true, -- This is mandatory, otherwise border config won't get registered.
-						handler_opts = {
-							border = "rounded",
-						},
-					}, bufnr)
-
 					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
 					-- to define small helper and utility functions so you don't have to repeat yourself.
 					--
@@ -91,15 +78,29 @@ return { -- LSP Configuration & Plugins
 					--  For example, in C this would take you to the header.
 					map("<leader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+					-- Automatically Setup signature Help
+					local bufnr = event.buf
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+					if vim.tbl_contains({ "null-ls" }, client.name) then -- blacklist lsp
+						return
+					end
+					require("lsp_signature").on_attach({
+						bind = true, -- This is mandatory, otherwise border config won't get registered.
+						handler_opts = {
+							border = "rounded",
+						},
+					}, bufnr)
+
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.server_capabilities.documentHighlightProvider then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 							buffer = event.buf,
 							group = highlight_augroup,
@@ -154,7 +155,6 @@ return { -- LSP Configuration & Plugins
 				bashls = {},
 				gopls = {},
 				pyright = {},
-				tsserver = {},
 				dockerls = {},
 				jsonls = {},
 				terraformls = {},
