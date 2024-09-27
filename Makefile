cd6a699c150fcabe55bb3f5f70cb4c2c53d3717d
Jesse Rusak <rusak.jesse@gmail.com>
2024-09-27 10:12:51 -0400

    feat(linux): stow under root

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+CUR_PLATFORM	:= $(shell uname)
+LINUX_PLATFORM	:= Linux
+
 all: sync install
 
 sync: 
 	stow */
+ifeq ($(CUR_PLATFORM), $(LINUX_PLATFORM))
+	sudo stow root -t /
+endif
 
 install: 
 	./install
--- editor/.config/nvim/lua/custom/plugins/git.lua
+++ editor/.config/nvim/lua/custom/plugins/git.lua
@@ -4,31 +4,31 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = {},
 	},
-	-- {
-	-- 	"NeogitOrg/neogit",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim", -- required
-	-- 		"sindrets/diffview.nvim", -- optional - Diff integration
-	-- 		"nvim-telescope/telescope.nvim", -- optional
-	-- 	},
-	-- 	config = function()
-	-- 		local neogit = require("neogit")
-	-- 		neogit.setup({
-	-- 			kind = "split",
-	-- 		})
-	--
-	-- 		vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Open NeoGit" })
-	-- 	end,
-	-- },
 	{
-		"kdheepak/lazygit.nvim",
+		"NeogitOrg/neogit",
 		dependencies = {
-			"nvim-lua/plenary.nvim",
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
 		},
 		config = function()
-			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
+			local neogit = require("neogit")
+			neogit.setup({
+				kind = "split",
+			})
+
+			vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Open NeoGit" })
 		end,
 	},
+	-- {
+	-- 	"kdheepak/lazygit.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- 	config = function()
+	-- 		vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
+	-- 	end,
+	-- },
 	{
 		"sindrets/diffview.nvim",
 	},
