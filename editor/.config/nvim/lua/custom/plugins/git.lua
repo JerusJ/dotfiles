add974244c0a40625d1107d5b64fa60ec2b029d0
Jesse Rusak <rusak.jesse@gmail.com>
2024-09-28 20:20:09 +0000

    chore(git): open neogit as vsplit not hsplit

--- editor/.config/nvim/lua/custom/plugins/git.lua
+++ editor/.config/nvim/lua/custom/plugins/git.lua
@@ -14,7 +14,7 @@ return {
 		config = function()
 			local neogit = require("neogit")
 			neogit.setup({
-				kind = "split",
+				kind = "vsplit",
 			})
 
 			vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Open NeoGit" })
